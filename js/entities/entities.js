c99a07d92bfbf94a270f6a5a63d2bf5f72d08b69
jonathan benelisha <jonathanbenelisha@gmail.com>
2015-01-22 11:51:28 -0800

    comments for first two videos

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,1 +1,10 @@
-// TODO
+game.PlayerEntinty.extend({
+	init: function(x, y, settings){
+		this_super(me.Entity, 'init', [x, y, {
+			image: "player",
+			width: 64,
+			height: 64, 
+		}]);
+	},
+
+});
--- js/resources.js
+++ js/resources.js
@@ -4,6 +4,9 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
+	 {name: "Background-tiles", type:"image", src: "data/img/background-tiles.png"},
+	 {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
+	 {name: "player", type:"image", src: "data/img/orcSpear.png"},
 
 	/* Atlases 
 	 * @example
@@ -15,6 +18,8 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
+ 	 {name: "level01", type: "tmx", src: "data/map/test.tmx"},
+
 
 	/* Background music. 
 	 * @example
