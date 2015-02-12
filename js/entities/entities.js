41ac4ed8b98ade1e0ab7dd36e6e4ad7e10a44976
jonathan benelisha <jonathanbenelisha@gmail.com>
2015-02-12 12:14:28 -0800

    video 22 making the creep able to hit the player and also making the creep stop for the player.

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -26,6 +26,8 @@ game.PlayerEntity = me.Entity.extend({
 		//moving 5 units to the right
 		//y is 20 so character is on the floor
 		//me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH); makes it so the character is always being followed on the screen
+		this.type = "PlayerEntity";
+		this.health = 20;
 		this.body.setVelocity(5, 20);
 		this.facing = "right";
 		this.now = new Date().getTime();
@@ -112,6 +114,11 @@ game.PlayerEntity = me.Entity.extend({
 		//this is updating the super class so the animations can update
 		this._super(me.Entity, "update", [delta]);
 		return true;
+	},
+
+	loseHealth: function(damage){
+		this.health = this.health - damage;
+		console.log(this.health);
 	},
 
 	collideHandler: function(response){
@@ -288,7 +295,14 @@ game.EnemyCreep = me.Entity.extend({
 	},
 
 	update: function(delta){
+
 		this.now = new Date() .getTime();
+
+		//if(this.body.vel.x == 0 && !this.body.jumping && !this.body.falling){
+			//this.body.jumping = true;
+			//this.body.vel.y -= this.body.accel.y * me.timer.tick;
+			
+		//}
 
 		this.body.vel.x -= this.body.accel.x * me.timer.tick;
 
@@ -310,6 +324,24 @@ game.EnemyCreep = me.Entity.extend({
 			this.pos.x = this.pos.x +1;
 			//checks that it has been at least 1 second since this creep hit a base
 			if((this.now-this.lastHit >= 1000)){
+				//updates the lasthit timer
+				this.lastHit = this.now;
+				//makes the player base call its losehealth function and passes it a damage of 1
+				response.b.loseHealth(1);
+			}
+		}else if(response.b.type==='PlayerEntity'){
+			var xdif = this.pos.x - response.b.pos.x;
+
+			this.attacking=true;
+			//this.lastAttacking=this.now;
+
+			if(xdif>0){
+			this.pos.x = this.pos.x +1;
+			this.body.vel.x = 0;
+			//keeps moving the creep to the right to maintain its position
+		}
+			//checks that it has been at least 1 second since this creep hit a base
+			if((this.now-this.lastHit >= 1000) && xdif>0){
 				//updates the lasthit timer
 				this.lastHit = this.now;
 				//makes the player base call its losehealth function and passes it a damage of 1
