86b7c742e08d6b2f1204539426b496b73305bcab
jonathan benelisha <jonathanbenelisha@gmail.com>
2015-02-20 19:13:09 -0800

    video 25 making the player respawn with errors

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -51,9 +51,6 @@ game.PlayerEntity = me.Entity.extend({
 
 		if (this.health <= 0) {
 			this.dead = true;
-			this.pos.x = 10;
-			this.pos.y = 0;
-			this.health = game.data.playerHealth;
 		}
 
 
@@ -400,6 +397,11 @@ game.EnemyCreep = me.Entity.extend({
 	},
 			update: function(){
 				this.now = new Date().getTime();
+
+				if(game.data.player.dead{
+					me.game.world.removeChild(game.data.player);
+					me.state.current().resetPlayer(10, 0);
+				}
 
 				if(Math.round(this.now/1000) % 10 ===0 && (this.now - this.lastCreep >= 1000)){
 					this.lastCreep = this.now;
