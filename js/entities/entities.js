8422c0e8568bff80f36bb3261a40ddf0340027c8
jonathan benelisha <jonathanbenelisha@gmail.com>
2015-02-27 19:31:42 -0800

    video 32 refactoring code

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -74,38 +74,13 @@ game.PlayerEntity = me.Entity.extend({
 
 	update: function(delta){
 		this.now = new Date().getTime();
-		if (this.health <= 0) {
-			this.dead = true;
-		};
 
+		this.dead = checkIfDead();
+
+		this.checkKeyPressedAndMove();
 
 		
-		//checking if the right key is pressed
-		if(me.input.isKeyPressed("right")){
-			//if the key is pressed this is what happens
-			//adds the position of my x by the velocity defined above in
-			//setVelocity() and multiplying it by me.timer.tick
-			//me.timer.tick makes the movement look smooth
-			this.body.vel.x += this.body.accel.x * me.timer.tick;
-			//this is so the character faces the right when moving to the right (if this isnt here the character faces the left when walking to the right)
-			this.facing = "right";
-			this.flipX(true);
-		}
-		//if you stop pressing the right key
-		else if (me.input.isKeyPressed("left")){
-			this.facing = "left";
-			this.body.vel.x -=this.body.accel.x * me.timer.tick;
-			this.flipX(false);
-		}
-		else{
-			//it wont move
-			this.body.vel.x = 0;
-		}
 
-		if(me.input.isKeyPressed("jump") && !this.body.falling && !this.body.jumping){
-			this.body.jumping = true;
-			this.body.vel.y -= this.body.accel.y * me.timer.tick;
-		} 
 
 		//checking if attack is pressed
 		if(me.input.isKeyPressed("attack")){
@@ -147,6 +122,55 @@ game.PlayerEntity = me.Entity.extend({
 		this._super(me.Entity, "update", [delta]);
 		return true;
 	},
+
+	checkIfDead: function(){
+		if (this.health <= 0){
+			return true;
+		}
+		return false;
+	},
+
+
+checkKeyPressedAndMove: function(){
+			//checking if the right key is pressed
+		if(me.input.isKeyPressed("right")){
+			this.moveRight();
+		}
+		//if you stop pressing the right key
+		else if (me.input.isKeyPressed("left")){
+			this.moveLeft();
+		}
+		else{
+			//it wont move
+			this.body.vel.x = 0;
+		}
+
+		if(me.input.isKeyPressed("jump") && !this.body.falling && !this.body.jumping){
+			this.jump();
+		} 
+},
+
+moveRight: function(){
+		//if the key is pressed this is what happens
+		//adds the position of my x by the velocity defined above in
+		//setVelocity() and multiplying it by me.timer.tick
+		//me.timer.tick makes the movement look smooth
+		this.body.vel.x += this.body.accel.x * me.timer.tick;			//this is so the character faces the right when moving to the right (if this isnt here the character faces the left when walking to the right)
+		this.facing = "right";
+		this.flipX(true);
+},
+
+moveLeft: function(){
+		this.facing = "left";
+		this.body.vel.x -=this.body.accel.x * me.timer.tick;
+		this.flipX(false);
+},
+
+jump: function(){
+		this.body.jumping = true;
+		this.body.vel.y -= this.body.accel.y * me.timer.tick;
+},
+
 
 	loseHealth: function(damage){
 		this.health = this.health - damage;
