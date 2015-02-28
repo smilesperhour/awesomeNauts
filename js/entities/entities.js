5a5fb3eb1ed60e4f2a0cb065759a3ee5410c22f6
jonathan benelisha <jonathanbenelisha@gmail.com>
2015-02-27 20:18:19 -0800

    video 33 refactor in collision

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -181,6 +181,12 @@ setAnimation: function(){
 
 	collideHandler: function(response){
 		if(response.b.type==='EnemyBaseEntity'){
+			this.collideWithEnemyBase(response);
+		}else if (response.b.type=== 'EnemyCreep') {
+			this.collideWithEnemyCreep(response);
+		}
+	},
+	collideWithEnemyBase: function(response){
 			var ydif = this.pos.y - response.b.pos.y;
 			var xdif = this.pos.x - response.b.pos.x;
 
@@ -194,7 +200,6 @@ setAnimation: function(){
 			}
 			else if(xdif<70 && this.facing==='left' && xdif>0){
 				this.body.vel.x = 0;
-				this.pos.x = this.pos.x +1;
 			}
 
 			if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >=1000){
@@ -202,27 +207,46 @@ setAnimation: function(){
 				response.b.loseHealth();
 			}
 
-		}else if (response.b.type=== 'EnemyCreep') {
+		},
+
+		collideWithEnemyCreep: function(response){
+
 			var xdif = this.pos.x - response.b.pos.x;
 			var ydif = this.pos.y - response.b.pos.y;
-			if (xdif>0){
-				this.pos.x = this.pos.x + 1;
+
+			this.stopMovement(xdif, ydif);
+			this.hitCreep(response);
+			};
+
+		},
+
+		stopMovement: function(xdif){
+						if (xdif>0){
 				if(this.facing === "left"){
 					this.body.vel.x = 0;
 				}
 			}else{
-				this.pos.x = this.pos - 1;
 				if(this.facing === "right"){
 					this.body.vel.x = 0;
 				}
 			}
 
+		},
+
+		checkAttack: function(xdif, ydif){
 			if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= game.data.playerAttackTimer
 					&& (Math.abs(ydif) <=40) && (((xdif>0 ) && this.facing==="left")
 					 || ((xdif<0) && this.facing==="right"))){
 				this.lastHit = this.now;
 			//if the creeps health is less than our attack, execute code if in statement
-			if(response.b.health <= game.data.playerAttack){
+			return true;
+			}
+			return false;
+		},
+
+		hitCreep: function(response){
+			if(this.checkAttack(xdif, ydif)){
+				if(response.b.health <= game.data.playerAttack){
 				//adds one gold for a creep kill
 				game.data.gold += 1;
 				console.log("Current gold: " + game.data.gold);
@@ -230,9 +254,8 @@ setAnimation: function(){
 
 
 				response.b.loseHealth(game.data.playerAttack);
-			}
 		}
-	}
+
 });
 
 
