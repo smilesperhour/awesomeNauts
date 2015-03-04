49243cfa0f2da1d7d0c87a143ac073ea81135588
jonathan benelisha <jonathanbenelisha@gmail.com>
2015-03-04 12:20:18 -0800

    Changing the title screen. making it so it needs to be clicked.

--- js/screens/title.js
+++ js/screens/title.js
@@ -6,28 +6,35 @@ game.TitleScreen = me.ScreenObject.extend({
 		 me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10);
 
 
-		 me.input.bindKey(me.input.KEY.ENTER, "start");
+		 
 
 		 me.game.world.addChild(new (me.Renderable.extend({
 
 		 	init: function(){
-		 		this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+		 		this._super(me.Renderable, 'init', [270, 240, 300, 50]);
 		 		this.font = new me.Font("Arial", 46, "white");
+		 		me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
 		 	},
 
 		 	draw: function(renderer){
-		 		this.font.draw(renderer.getContext(), "Awesomenauts!", 450, 130);
-		 		this.font.draw(renderer.getContext(), "press ENTER to play!", 250, 530);
-		 	}
+		 		this.font.draw(renderer.getContext(), "START A NEW GAME", this.pos.x, this.pos.y);
+		 	},
 
-		 })));
+		 	update: function(dt){
+		 		return true;
+		 	},
 
-		 this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keyCode, edge) {
-		 	if(action === "start"){
+		 	newGame: function(){
+		 		me.input.releasePointerEvent('pointerdown', this);
+		 		me.save.remove('exp');
+		 		me.save.remove('exp1');
+		 		me.save.remove('exp2');
+		 		me.save.remove('exp3');
+		 		me.save.remove('exp4');
 		 		me.state.change(me.state.PLAY);
 		 	}
 
-		 })
+		 })));
 
 	}, 
 	
@@ -36,7 +43,6 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		me.input.unbindKey(me.input.KEY.ENTER);
-		me.event.unsubscribe(this.handler);
+
 	}
 });
