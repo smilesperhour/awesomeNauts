2912dfe0b3ed126d0f5656efeedff38ba6e36a11
jonathan benelisha <jonathanbenelisha@gmail.com>
2015-04-01 18:31:35 -0700

    video 52 making new load screens

--- js/resources.js
+++ js/resources.js
@@ -12,6 +12,10 @@ game.resources = [
 	 {name: "title-screen", type:"image", src: "data/img/title.png"},
 	 {name: "exp-screen", type:"image", src: "data/img/loadpic.png"},
 	 {name: "gold-screen", type:"image", src: "data/img/spend.png"},
+	 {name: "minimap", type:"image", src: "data/img/minimap.png"},
+	 {name: "load-screen", type:"image", src: "data/img/loadpic.png"},
+	 {name: "new-screen", type:"image", src: "data/img/newpic.png"},
+
 
 
 	/* Atlases 
